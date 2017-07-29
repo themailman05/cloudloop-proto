85598add6bb7f66ce8e5eb0cd270e650cd1f36c5
Liam Sargent <themailman05@gmail.com>
2017-07-29 12:56:16 -0700

    first commit

---
+++ install_debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo aptitude install libopusfile-dev opus-tools libopus-dev libortp-dev libclalsadrv-dev
+
+if [[ -f /usr/lib/libopusfile.so ]]; then
+  echo "opusfile installed."
+  #TODO check other require
+  git clone http://www.pogo.org.uk/~mark/trx.git
+  cd trx
+  make
+  sudo make install
+fi
+
+echo `which opusenc`
+echo "Done!"
